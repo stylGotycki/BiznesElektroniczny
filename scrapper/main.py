e9c0d96426b19c2e03846b0a656acc589f6d3e7f
stylGotycki <stylgotycki@o2.pl>
2025-11-21 23:45:21 +0100

    scrapping a description works!

--- scrapper/main.py
+++ scrapper/main.py
@@ -13,7 +13,7 @@ class ProductView:
     
     def __init__(self, name: str, link: str, images: list | None, price: float, manufacturer: str):
         """
-        Konstuktor
+        Konstuktor.
 
         Args:
             name (str): _description_
@@ -22,6 +22,7 @@ class ProductView:
             price (float): _description_
             manufacturer (str): _description_
         """
+        
         self.name = name
         self.link = link
         self.images = []
@@ -29,7 +30,14 @@ class ProductView:
         self.manufacturer = manufacturer
         self.description = None
         
-    def to_dict(self):
+    def to_dict(self) -> dict:
+        """
+        Zwraca obiekt jako słownik.
+
+        Returns:
+            dict: Obiekt jako słownik.
+        """
+        
         return {
             "name": self.name,
             "images": self.images,
@@ -38,8 +46,27 @@ class ProductView:
             "description": self.description
         }
     
-    def scrape_product_details():
-        pass
+    def scrape_product_details(self, images_dir: str):
+        """
+        Scrapping informacji o produkcie. Następnie aktualizacja obiektu i zapis zdjęć we wskazanym katalogu.
+
+        Args:
+            images_dir (str): Nazwa katalogu, w którym będą zapisane zdjęcia produktu.
+        """
+        
+        full_url = f"https://iklamki.pl/pl/produkty/{self.link}"
+        response = requests.get(full_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        description_element = soup.find('div', class_='product-description', itemprop='description')
+        if description_element:
+            paragraphs = description_element.find_all('p')
+            self.description = [p.get_text(strip=True) for p in paragraphs if p.get_text(strip=True)]
+        else:
+            self.description = ["Brak opisu"]
+                
+        # print(description_element)
+        # print(self.description)
 
     
 def get_html_with_requests(url: str):
@@ -57,3 +84,20 @@ def get_html_with_requests(url: str):
 
 class Category:
     pass
+
+
+def main():
+    # now only for testing purposes :P
+    product = ProductView(
+        name = 'for now empty',
+        link = '2764-uchwyt-meblowy-viefe-rille-55mm-stal-szlachetna.html', # for now hardcoded
+        images = [],
+        price = 21.37,
+        manufacturer = 'for now empty'
+    )
+    
+    product.scrape_product_details('for now empty')
+
+
+if __name__ == "__main__":
+    main()
