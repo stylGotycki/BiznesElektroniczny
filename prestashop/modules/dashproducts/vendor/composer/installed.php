f64aa2af0e3b00dd2ab5b2fbaf067c7c786af4a8
stylGotycki <stylgotycki@o2.pl>
2025-11-23 21:14:51 +0100

    pull requests issues resolved?

---
+++ prestashop/modules/dashproducts/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1e126b6ce089b5ee6c45eb5b87c5534c5348964b',
+        'name' => 'prestashop/dashproducts',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/dashproducts' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1e126b6ce089b5ee6c45eb5b87c5534c5348964b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ prestashop/modules/ps_categorytree/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '83222e414ef3e2e2a20b1b9758f3a615ba917bf3',
+        'name' => 'prestashop/ps_categorytree',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/ps_categorytree' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '83222e414ef3e2e2a20b1b9758f3a615ba917bf3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
