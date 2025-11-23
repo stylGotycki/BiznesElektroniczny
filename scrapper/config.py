bce5b040084d2e158d04a78af18fcdd1a491565b
Domaszk Filip <90199265+stylGotycki@users.noreply.github.com>
2025-11-23 21:36:57 +0100

    Revert "feature/scraper implemented"

--- scrapper/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import base64
-
-API_KEY = 'BJK9VTGMIKT6JG655I8NVFIJK7S74ZUJ'
-API_URL = 'http://localhost:8080/api'
-
-HEADERS = {
-    'Authorization': f'Basic {base64.b64encode( f"{API_KEY}:".encode() ).decode()}'
-}
